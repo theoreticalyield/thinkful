c032d6233be0ca7a837d6f146cdf166307dd70d7
David Hill <davidandhill@gmail.com>
2014-03-16 12:35:59 -0700

    first thinkful tip calculator project

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -9,8 +9,8 @@ meal_with_tax = meal + (meal * tax)
 tip_value = meal * tip
 total_cost = meal_with_tax + tip_value
 
-puts "The pretax value of your meal is $#{'%.2f' % tax_value.to_f}."
-puts "At a tax rate of 8%, the tax for this meal is $#{'%.2f' % meal_with_tax}."
+puts "The pretax value of your meal is $#{'%.2f' % meal.to_f}."
+puts "At a tax rate of 8%, the tax for this meal is $#{'%.2f' % tax_value}."
 puts "A tip at 20% makes for a tip amount of $#{'%.2f' % tip_value}."
 puts "So the grand total with tax and tip comes to $#{ '%.2f' % total_cost}."
 
